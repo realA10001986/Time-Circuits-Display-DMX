d54875da619a23c40c782ac2e02b61d7ea1f3eef
A10001986 <76924199+realA10001986@users.noreply.github.com>
2024-02-24 00:32:21 +0100

    Add files via upload

---
+++ tcd-DMX/tcd-DMX.ino
@@ -0,0 +1,100 @@
+/*
+ * -------------------------------------------------------------------
+ * CircuitSetup.us Time Circuits Display - DMX-controlled
+ * (C) 2024 Thomas Winischhofer (A10001986)
+ * All rights reserved.
+ * -------------------------------------------------------------------
+ */
+
+/*
+ * Build instructions (for Arduino IDE)
+ * 
+ * - Install the Arduino IDE
+ *   https://www.arduino.cc/en/software
+ *    
+ * - This firmware requires the "ESP32-Arduino" framework. To install this framework, 
+ *   in the Arduino IDE, go to "File" > "Preferences" and add the URL   
+ *   https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+ *   - or (if the URL above does not work) -
+ *   https://espressif.github.io/arduino-esp32/package_esp32_index.json
+ *   to "Additional Boards Manager URLs". The list is comma-separated.
+ *   
+ * - Go to "Tools" > "Board" > "Boards Manager", then search for "esp32", and install 
+ *   the latest version by Espressif Systems.
+ *   Detailed instructions for this step:
+ *   https://docs.espressif.com/projects/arduino-esp32/en/latest/installing.html
+ *   
+ * - Go to "Tools" > "Board: ..." -> "ESP32 Arduino" and select your board model (the
+ *   CircuitSetup original boards are "NodeMCU-32S")
+ *   
+ * - Connect your ESP32 board using a suitable USB cable.
+ *   Note that NodeMCU ESP32 boards come in two flavors that differ in which serial 
+ *   communications chip is used: Either SLAB CP210x USB-to-UART or CH340. Installing
+ *   a driver might be required.
+ *   Mac: 
+ *   For the SLAB CP210x (which is used by NodeMCU-boards distributed by CircuitSetup)
+ *   installing a driver is required:
+ *   https://www.silabs.com/developers/usb-to-uart-bridge-vcp-drivers?tab=downloads
+ *   The port ("Tools -> "Port") is named /dev/cu.SLAB_USBtoUART, and the maximum
+ *   upload speed ("Tools" -> "Upload Speed") can be used.
+ *   The CH340 is supported out-of-the-box since Mojave. The port is named 
+ *   /dev/cu.usbserial-XXXX (XXXX being some random number), and the maximum upload 
+ *   speed is 460800.
+ *   Windows:
+ *   For the SLAB CP210x (which is used by NodeMCU-boards distributed by CircuitSetup)
+ *   installing a driver is required:
+ *   https://www.silabs.com/developers/usb-to-uart-bridge-vcp-drivers?tab=downloads
+ *   After installing this driver, connect your ESP32, start the Device Manager, 
+ *   expand the "Ports (COM & LPT)" list and look for the port with the ESP32 name.
+ *   Choose this port under "Tools" -> "Port" in Arduino IDE.
+ *   For the CH340, another driver is needed. Try connecting the ESP32 and have
+ *   Windows install a driver automatically; otherwise search google for a suitable
+ *   driver. Note that the maximum upload speed is either 115200, or perhaps 460800.
+ *
+ * - Install required libraries. In the Arduino IDE, go to "Tools" -> "Manage Libraries" 
+ *   and install the following libraries:
+ *   - esp_dmx (>= 4.0.1): https://github.com/someweisguy/esp_dmx
+ *
+ * - Download the complete firmware source code:
+ *   Extract this file somewhere. Enter the "tcd-DMX" folder and 
+ *   double-click on "tcd-DMX.ino". This opens the firmware in the
+ *   Arduino IDE.
+ *
+ * - Go to "Sketch" -> "Upload" to compile and upload the firmware to your ESP32 board.
+ *
+ */
+
+
+#include "tc_global.h"
+
+#include <Arduino.h>
+#include <Wire.h>
+
+//#include "tc_keypad.h"
+#include "tc_settings.h"
+#include "tc_dmx.h"
+
+void setup()
+{
+    powerupMillis = millis();
+    
+    Serial.begin(115200);
+    Serial.println();
+
+    // I2C init
+    // Make sure our i2c buf is 128 bytes
+    Wire.setBufferSize(128);
+    // PCF8574 only supports 100kHz, can't go to 400 here.
+    Wire.begin(-1, -1, 100000);
+
+    dmx_boot();
+    settings_setup();
+    //keypad_setup();
+    dmx_setup();
+}
+
+
+void loop()
+{
+    dmx_loop();
+}
