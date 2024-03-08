84606063f294800d0ad876eacca5c8a6242b4da9
A10001986 <76924199+realA10001986@users.noreply.github.com>
2024-03-08 14:04:47 +0100

    Disable debug output

--- tcd-DMX/tc_global.h
+++ tcd-DMX/tc_global.h
@@ -18,7 +18,7 @@
 #define TC_VERSION "V0.09"
 #define TC_VERSION_EXTRA "MAR072024"
 
-#define TC_DBG              // debug output on Serial
+//#define TC_DBG              // debug output on Serial
 
 // If this is uncommented, the firmware uses channel DMX_VERIFY_CHANNEL
 // for packet verification. The value of this channel must, at all times,
